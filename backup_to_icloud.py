6e26b25473c3c7596c259d61e29e2feabc17fbd4
Alex Leigh <daleighan@gmail.com>
2017-11-30 16:50:52 -0500

    Files are now read from the documents folder

--- backup_to_icloud.py
+++ backup_to_icloud.py
@@ -3,8 +3,17 @@
 import os
 
 def backup():
-    files = os.listdir('/')
-    print(files)
+    documents_files = os.listdir('/users/leighn/Documents')
+    #print(files)
+    icloud_files_documents = os.listdir('/users/leighn/iCloudDrive/Documents')
+    documents_to_add = []
+
+    for file in documents_files:
+        if file not in icloud_files_documents:
+            documents_to_add.append(file)
+
+    print(documents_to_add)
+
 
 if __name__ == "__main__":
     print("Backing up files to icloud")
